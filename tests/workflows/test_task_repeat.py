738b5104c67bef9cb1a0cec71764ff6a4befac4f
Ethan Swan <ethanpswan@gmail.com>
2024-03-17 14:07:52 -0500

    Start repeating task test.

--- tests/workflows/test_task_repeat.py
+++ tests/workflows/test_task_repeat.py
@@ -1,8 +1,4 @@
 import re
-from datetime import datetime
-
-from tzlocal import get_localzone
-from zoneinfo import ZoneInfo
 
 from now_and_here.datastore import DataStore
 
@@ -11,17 +7,18 @@ from .nhrunner import NHRunner
 # TODO: incorporate freezegun
 
 
-def test_task_due(temp_store: DataStore, nh: NHRunner):
-    """Test that task due dates are stored correctly."""
+def test_task_repeat(temp_store: DataStore, nh: NHRunner):
+    """Test that repeating tasks repeat correctly."""
+    # Create a repeating task.
     args = ["task", "add", "-i"]
     input = (
         "\n".join(
             [
-                "test_due_task",  # name
+                "test_repeating_task",  # name
                 "",  # description
                 "1",  # priority
                 "today 8pm",  # due date
-                "",  # repeat (None)
+                "every wednesday at 3:17",  # repeat (None)
                 "no",  # is this task part of a project?
             ]
         )
@@ -39,14 +36,9 @@ def test_task_due(temp_store: DataStore, nh: NHRunner):
     assert match is not None
     id = match.group("id").replace("-", "")
 
-    # Make sure the task's due date was stored in UTC.
-    task = temp_store.get_task(id)
-    now = datetime.now().astimezone(get_localzone())
-    today_8pm_local = now.replace(hour=20, minute=0, second=0, microsecond=0)
-    today_8pm_in_utc = today_8pm_local.astimezone(ZoneInfo("UTC"))
-    assert task.due == today_8pm_in_utc
-
     # But also make sure that it renders correctly when printed to the console.
-    result = nh.invoke(["task", "get", id, "--field", "due"])
+    result = nh.invoke(["task", "get", id, "--field", "repeat"])
     assert result.exit_code == 0
-    assert result.stdout.strip() == today_8pm_local.strftime("%Y-%m-%d %H:%M")
+    expected_output = "WeeklyInterval(weeks=1,weekdays={<Weekday.WEDNESDAY:2>},at=datetime.time(3,17))"
+    output = result.stdout.replace(" ", "").replace("\n", "").replace("\t", "")
+    assert expected_output == output
