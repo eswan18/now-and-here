f0cf6764bac1205b51ad3f537261236cb9df60d8
Ethan Swan <ethanpswan@gmail.com>
2024-03-29 23:10:07 -0500

    Clean up web UI.

--- now_and_here/fastapi_app/app.py
+++ now_and_here/fastapi_app/app.py
@@ -1,10 +1,11 @@
+import os
 import subprocess
 from contextlib import asynccontextmanager
 from pathlib import Path
 from typing import Annotated
 
 from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -14,28 +15,27 @@ from now_and_here.datastore.errors import RecordNotFoundError
 TEMPLATE_DIR = Path(__file__).parent / "templates"
 STATIC_DIR = Path(__file__).parent / "static"
 
+NH_BUILD_TW = os.getenv("NH_BUILD_TW") in ("1", "True", "true")
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     """Context manager for FastAPI app. It will run all code before `yield`
     on app startup, and will run code after `yield` on app shutdown.
     """
-    try:
+    if NH_BUILD_TW:
         print("Running tailwindcss...")
         subprocess.run(
             [
                 "tailwindcss",
                 "-i",
-                "now_and_here/fastapi_app/static/src/tw.css",
+                STATIC_DIR / "src" / "tw.css",
                 "-o",
-                "now_and_here/fastapi_app/static/css/main.css",
+                STATIC_DIR / "css" / "main.css",
                 "--minify",
             ]
         )
         print("done")
-    except Exception as e:
-        print("Error running tailwindcss...")
-        raise e
     yield
 
 
@@ -44,12 +44,13 @@ templates = Jinja2Templates(directory=TEMPLATE_DIR)
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=RedirectResponse)
 def index(request: Request):
-    return templates.TemplateResponse("main.html", {"request": request})
+    # Redirect to /projects
+    return RedirectResponse(url="/projects")
 
 
-@app.get("/task/{id}", response_class=HTMLResponse)
+@app.get("/tasks/{id}", response_class=HTMLResponse)
 def get_task(request: Request, id: str):
     store = datastore.get_store()
     try:
@@ -60,7 +61,7 @@ def get_task(request: Request, id: str):
     return templates.TemplateResponse("task.html", {"request": request, "task": task})
 
 
-@app.post("/task/{id}")
+@app.post("/tasks/{id}")
 def post_task(id: str, done: Annotated[bool | None, Form()] = None):
     print(done)
     store = datastore.get_store()
@@ -72,7 +73,16 @@ def post_task(id: str, done: Annotated[bool | None, Form()] = None):
     return None
 
 
-@app.get("/project/{id}", response_class=HTMLResponse)
+@app.get("/projects", response_class=HTMLResponse)
+def projects(request: Request):
+    store = datastore.get_store()
+    projects = store.get_projects()
+    return templates.TemplateResponse(
+        "projects.html", {"request": request, "projects": projects}
+    )
+
+
+@app.get("/projects/{id}", response_class=HTMLResponse)
 def project(request: Request, id: str, sort_by: str = "due", desc: bool = False):
     store = datastore.get_store()
     try:
