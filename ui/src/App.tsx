78337aea730249f71f123e2ef0ed2718c8941527
Ethan Swan <ethanpswan@gmail.com>
2024-04-10 22:51:07 -0500

    Fix for typescript.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
   return (
@@ -13,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
+          Hi!!
         </p>
         <a
           className="App-link"
