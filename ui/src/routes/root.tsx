3f6260781f24efcf4893794460f2d243aba56ee7
Ethan Swan <ethanpswan@gmail.com>
2024-04-10 23:55:19 -0500

    Add some elements of old app.

--- ui/src/routes/root.tsx
+++ ui/src/routes/root.tsx
@@ -1,47 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export default function Root() {
   return (
-    <>
-      <div id="sidebar" className="bg-red-500">
-        <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div
-              id="search-spinner"
-              aria-hidden
-              hidden={true}
-            />
-            <div
-              className="sr-only"
-              aria-live="polite"
-            ></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
+    <div className="flex flex-col justify-start w-full min-h-screen bg-gray-50">
+      <div className="w-full px-4 h-10">
+        <div className="flex flex-row justify-between items-center h-full w-full px-4 lg:px-8">
+          <h1 className="text-base lg:text-lg font-bold text-orange-800"><Link to="/">Now and Here</Link></h1>
+          <div className="text-sm lg:text-base flex flex-row text-orange-900 gap-4">
+            <a href="#">Projects</a>
+            <a href="#">Views</a>
+          </div>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/contacts/1`}>Your Name</a>
-            </li>
-            <li>
-              <a href={`/contacts/2`}>Your Friend</a>
-            </li>
-          </ul>
-        </nav>
       </div>
-      <div id="detail">
-        <Outlet />
+      <div className="w-full h-16 lg:h-28 lg:py-4 bg-orange-800">
+        <div className="lg:max-w-[60rem] mx-auto p-4 lg:p-8 text-orange-50" id="headerContainer">
+          <Outlet />
+        </div>
       </div>
-    </>
+      <div className="w-full">
+        <div className="lg:max-w-[60rem] mx-auto px-4 lg:px-8" id="mainContentContainer">
+          <Outlet />
+        </div>
+      </div>
+    </div>
   );
 }
