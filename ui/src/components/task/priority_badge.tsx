52ad91931552c55117c2af759c69f0393604068e
Ethan Swan <ethanpswan@gmail.com>
2024-05-24 15:30:04 -0500

    Add task dialog (#15)
    * Add modal when tasks are clicked on.
    
    * Add a "more" icon on task cards.

--- ui/src/components/task/priority_badge.tsx
+++ ui/src/components/task/priority_badge.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { TriangleAlert } from "lucide-react";
 
 export default function PriorityBadge({ priority }: { priority: number }) {
   let colors = null;
@@ -17,6 +18,37 @@ export default function PriorityBadge({ priority }: { priority: number }) {
       break;
   }
   return (
-    <Badge className={`inline-block w-20 ${colors}`}>Priority {priority}</Badge>
+    <Badge
+      className={`inline-block w-24 ${colors} flex flex-row items-center justify-center gap-1`}
+    >
+      <TriangleAlert size={14} className="inline" />
+      Priority {priority}
+    </Badge>
+  );
+}
+
+export function BigPriorityBadge({ priority }: { priority: number }) {
+  let colors = null;
+  switch (priority) {
+    case 3:
+      colors = "bg-red-200 hover:bg-red-200 text-red-900";
+      break;
+    case 2:
+      colors = "bg-orange-200 hover:bg-orange-200 text-orange-900";
+      break;
+    case 1:
+      colors = "bg-yellow-200 hover:bg-yellow-200 text-yellow-900";
+      break;
+    case 0:
+      colors = "bg-blue-200 hover:bg-blue-200 text-blue-900";
+      break;
+  }
+  return (
+    <Badge
+      className={`inline-block w-32 ${colors} text-base flex flex-row items-center justify-center gap-1.5`}
+    >
+      <TriangleAlert size={16} />
+      Priority {priority}
+    </Badge>
   );
 }
--- ui/src/components/task/task_card.tsx
+++ ui/src/components/task/task_card.tsx
@@ -1,4 +1,4 @@
-import { Clock } from "lucide-react";
+import { Clock, Ellipsis, FolderOpen } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import {
   HoverCard,
@@ -10,6 +10,8 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { relativeTimeString } from "@/lib/time";
 import { Task } from "@/types/task";
 import PriorityBadge from "./priority_badge";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import TaskDialog from "./task_dialog";
 
 interface TaskCardProps {
   task: Task;
@@ -18,59 +20,66 @@ interface TaskCardProps {
 
 export default function TaskCard({ task, onToggleCompletion }: TaskCardProps) {
   return (
-    <Card>
-      <CardContent className="px-4 py-3">
-        <div className="flex flex-row flex-wrap w-full justify-between pl-12 mb-1">
-          <div className="text-sm font-semibold text-orange-700">
-            {task.project && (
-              <a href={`/projects/${task.project.id}`}>{task.project.name}</a>
-            )}
-          </div>
-          <div className="">
+    <Dialog>
+      <Card>
+        <CardContent className="px-4 py-3">
+          <div className="flex flex-row flex-wrap w-full justify-between pl-12 pr-4 mb-1">
+            <div className="text-sm font-semibold text-orange-700">
+              {task.project && (
+                <div className="flex flex-row justify-start items-center gap-2">
+                  <FolderOpen size={18} />
+                  <a href={`/projects/${task.project.id}`}>
+                    {task.project.name}
+                  </a>
+                </div>
+              )}
+            </div>
             <PriorityBadge priority={task.priority} />
           </div>
-        </div>
-        <div className="flex flex-row justify-between items-center w-full border-b border-b-gray-200">
-          <div className="flex flex-row items-center justify-start gap-2 pl-4">
-            <Checkbox
-              checked={task.done}
-              onCheckedChange={() => onToggleCompletion(task.id, !task.done)}
-              className="h-5 w-5 mr-1.5 border border-gray-400"
-            />
-            <div className="flex flex-row items-center justify-start gap-4">
-              <h3 className="font-semibold text-lg inline-block">
-                <a href={`/tasks/${task.id}`}>{task.name}</a>
-              </h3>
+          <div className="flex flex-row justify-between items-center w-full border-b border-b-gray-200 px-4">
+            <div className="flex flex-row items-center justify-start gap-2">
+              <Checkbox
+                checked={task.done}
+                onCheckedChange={() => onToggleCompletion(task.id, !task.done)}
+                className="h-5 w-5 mr-1.5 border border-gray-400"
+              />
+              <DialogTrigger>
+                <h3 className="font-semibold text-lg">
+                  {task.name}
+                  <Ellipsis className="ml-2 inline text-gray-500" />
+                </h3>
+              </DialogTrigger>
             </div>
+            {task.due ? (
+              <HoverCard>
+                <HoverCardTrigger>
+                  <Badge
+                    variant="outline"
+                    className="flex flex-row items-center text-orange-800"
+                  >
+                    <Clock size={16} className="inline-block mr-1" />
+                    <p>{relativeTimeString(new Date(task.due))}</p>
+                  </Badge>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-48">
+                  <div className="flex flex-col items-center">
+                    <p className="text-xs text-gray-400">Due:</p>
+                    <p className="text-sm text-gray-800">
+                      {new Date(task.due).toLocaleString()}
+                    </p>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
+            ) : (
+              <p className="text-gray-400 text-xs mr-2">No due date</p>
+            )}
           </div>
-          {task.due ? (
-            <HoverCard>
-              <HoverCardTrigger>
-                <Badge
-                  variant="outline"
-                  className="flex flex-row items-center text-orange-800"
-                >
-                  <Clock size={16} className="inline-block mr-1" />
-                  <p>{relativeTimeString(new Date(task.due))}</p>
-                </Badge>
-              </HoverCardTrigger>
-              <HoverCardContent className="w-48">
-                <div className="flex flex-col items-center">
-                  <p className="text-xs text-gray-400">Due:</p>
-                  <p className="text-sm text-gray-800">
-                    {new Date(task.due).toLocaleString()}
-                  </p>
-                </div>
-              </HoverCardContent>
-            </HoverCard>
-          ) : (
-            <p className="text-gray-400 text-sm mr-2">No due date</p>
-          )}
-        </div>
-        <div className="flex flex-row flex-wrap w-full justify-between pl-14 pt-1">
-          <div className="text-gray-400 text-sm">{task.description}</div>
-        </div>
-      </CardContent>
-    </Card>
+          <div className="flex flex-row flex-wrap w-full justify-between pl-14 pt-1">
+            <div className="text-gray-400 text-sm">{task.description}</div>
+          </div>
+        </CardContent>
+      </Card>
+      <TaskDialog task={task} onToggleCompletion={onToggleCompletion} />
+    </Dialog>
   );
 }
